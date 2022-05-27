677b893300df80fd9b0b1310eb28731f584a446e
shuntagami <shun.tagami@i-nations.tokyo>
2022-05-27 23:40:53 +0900

    $ docker compose run --rm app php artisan migrate

--- database/migrations/2022_05_27_082320_create_memos_table.php
+++ database/migrations/2022_05_27_082320_create_memos_table.php
@@ -14,8 +14,17 @@ return new class extends Migration
     public function up()
     {
         Schema::create('memos', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            // unsigned(±なし)にしないと外部キー制約に使えないのでid周りは全てunsignedをつける
+            // 第二引数 trueはauto incrementの意
+            $table->unsignedBigInteger('id', true);
+            $table->longText('content');
+            $table->unsignedBigInteger('user_id');
+            // 論理削除を定義→deleted_atを自動生成
+            $table->softDeletes();
+            // timestampと書いてしまうと、レコード挿入時、更新時に値が入らないので、DB::rawで直接書いてます
+            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
