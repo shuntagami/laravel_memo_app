a6e45207f3d899ce1b25f0b979783f3ca45bc737
shuntagami <shun.tagami@i-nations.tokyo>
2022-05-28 14:43:06 +0900

    Update Tag

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -47,10 +47,13 @@ class HomeController extends Controller
         DB::transaction(function () use($posts) {
             $memo_id = Memo::insertGetId(['content' => $posts['content'], 'user_id' => \Auth::id()]);
             $tag_exists = Tag::where('user_id', '=', \Auth::id())->where('name', '=', $posts['new_tag'])->exists();
+
+            // 新規タグが入力されているかチェック(もしあればinsert)
             if (!empty($posts['new_tag']) && !$tag_exists) {
                 $tag_id = Tag::insertGetId(['user_id' => \Auth::id(), 'name' => $posts['new_tag']]);
                 MemoTag::insert(['memo_id' => $memo_id, 'tag_id' => $tag_id]);
             }
+
             if(!empty($posts['tags'][0])){
                 foreach($posts['tags'] as $tag) {
                     MemoTag::insert(['memo_id' => $memo_id, 'tag_id' => $tag]);
@@ -69,9 +72,23 @@ class HomeController extends Controller
             ->orderBy('updated_at', 'DESC')
             ->get();
 
-        $edit_memo = Memo::find($id);
+        $edit_memo = Memo::select('memos.*', 'tags.id AS tag_id')
+            ->leftJoin('memo_tags', 'memo_tags.memo_id', '=', 'memos.id')
+            ->leftJoin('tags', 'memo_tags.tag_id', '=', 'tags.id')
+            ->where('memos.user_id', '=', \Auth::id())
+            ->where('memos.id', '=', $id)
+            ->whereNull('memos.deleted_at')
+            ->get();
 
-        return view('edit', compact('memos', 'edit_memo'));
+
+        $include_tags = [];
+        foreach($edit_memo as $memo) {
+            array_push($include_tags, $memo['tag_id']);
+        }
+
+        $tags = Tag::where('user_id', '=', \Auth::id())->whereNull('deleted_at')->orderBy('id', 'DESC')->get();
+
+        return view('edit', compact('memos', 'edit_memo', 'include_tags', 'tags'));
     }
 
     public function update(Request $request)
@@ -80,7 +97,26 @@ class HomeController extends Controller
         // dump die
         // dd($posts);
 
-        Memo::where('id', $posts['memo_id'])->update(['content' => $posts['content']]);
+        DB::transaction(function () use($posts) {
+            Memo::where('id', $posts['memo_id'])->update(['content' => $posts['content']]);
+
+            // 一旦中間テーブルのデータを削除
+            MemoTag::where('memo_id', '=', $posts['memo_id'])->delete();
+
+            // 再度メモとタグの紐付け
+            if (!empty($posts['tags'])) {
+                foreach($posts['tags'] as $tag) {
+                    MemoTag::insert(['memo_id' => $posts['memo_id'], 'tag_id' => $tag]);
+                }
+            }
+
+            // 新規タグが入力されているかチェック(もしあればinsert)
+            $tag_exists = Tag::where('user_id', '=', \Auth::id())->where('name', '=', $posts['new_tag'])->exists();
+            if (!empty($posts['new_tag']) && !$tag_exists) {
+                $tag_id = Tag::insertGetId(['user_id' => \Auth::id(), 'name' => $posts['new_tag']]);
+                MemoTag::insert(['memo_id' => $posts['memo_id'], 'tag_id' => $tag_id]);
+            }
+        });
 
         return redirect(route('home'));
     }
