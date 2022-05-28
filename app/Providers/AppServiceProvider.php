70764540f188f06e8c7f6cdee7b2813b7c719d6e
shuntagami <shun.tagami@i-nations.tokyo>
2022-05-28 17:44:37 +0900

    Refactoring search logic by query builder

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,23 +27,9 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         view()->composer('*', function ($view) {
-            $query_tag = \Request::query('tag');
-            // クエリパラメータtagがあればmemoをタグで絞り込み
-            if (!empty($query_tag)) {
-                $memos = Memo::select('memos.*')
-                    ->leftJoin('memo_tags', 'memo_tags.memo_id', '=', 'memos.id')
-                    ->where('memo_tags.tag_id', '=', $query_tag)
-                    ->where('user_id', '=', \Auth::id())
-                    ->whereNull('deleted_at')
-                    ->orderBy('updated_at', 'DESC')
-                    ->get();
-            } else {
-                $memos = Memo::select('memos.*')
-                    ->where('user_id', '=', \Auth::id())
-                    ->whereNull('deleted_at')
-                    ->orderBy('updated_at', 'DESC')
-                    ->get();
-            }
+            $memo_model = new Memo();
+            $memos = $memo_model->getMyMemo();
+
             $tags = Tag::where('user_id', '=', \Auth::id())
                 ->whereNull('deleted_at')
                 ->orderBy('id', 'DESC')
