f09fd69a14e20237401efa446d3afe15e00b3f3b
shuntagami <shun.tagami@i-nations.tokyo>
2022-05-28 12:47:01 +0900

    Edit memo

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -43,4 +43,28 @@ class HomeController extends Controller
 
         return redirect(route('home'));
     }
+
+    public function edit($id)
+    {
+        $memos = Memo::select('memos.*')
+            ->where('user_id', '=', \Auth::id())
+            ->whereNull('deleted_at')
+            ->orderBy('updated_at', 'DESC')
+            ->get();
+
+        $edit_memo = Memo::find($id);
+
+        return view('edit', compact('memos', 'edit_memo'));
+    }
+
+    public function update(Request $request)
+    {
+        $posts = $request->all();
+        // dump die
+        // dd($posts);
+
+        Memo::where('id', $posts['memo_id'])->update(['content' => $posts['content']]);
+
+        return redirect(route('home'));
+    }
 }
